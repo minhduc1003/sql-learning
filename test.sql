2e8f89469e2df7d3758f0a5004651c05afecf049
Minh Duc <vuminhduc231003@gmail.com>
2024-02-28 00:04:28 +0700

    learn day 2

--- test.sql
+++ test.sql
@@ -1,4 +1,12 @@
 SET SERVEROUTPUT ON;
+--CREATE TABLE employees (
+--    employee_id NUMBER(10),
+--    employee_name VARCHAR2(20),
+--    employee_slary NUMBER(10)
+--);
+--INSERT INTO employees values (3,'ducdz',3000)
+--select * from employees
+-------------------------------------
 --DECLARE 
 --   slary employees.employee_slary%type;
 --   name employees.employee_name%type;
@@ -7,17 +15,65 @@ SET SERVEROUTPUT ON;
 --     select employee_slary, employee_name into slary,name from employees where employee_id=1;
 --     DBMS_OUTPUT.PUT_LINE('slary is: '||slary||' name is: '||name);
 --END;
---CREATE TABLE employees (
---    employee_id NUMBER(10),
---    employee_name VARCHAR2(20),
---    employee_slary NUMBER(10)
---);
---INSERT INTO employees values (1,'duc',1000)
---select * from employees
-
-variable name varchar2(10);
-exec :name := 'duc';
-begin 
-:name := 'duc1';
-dbms_output.put_line(:name);
-end;
+-------------------------------------
+--variable name varchar2(10);
+--exec :name := 'duc';
+--begin 
+--:name := 'duc1';
+--dbms_output.put_line(:name);
+--end;
+--------------------------------------
+--DECLARE
+--    v_num NUMBER:= &a; 
+--    
+--begin
+--    if v_num>0 then
+--    dbms_output.put_line('1');
+--    else 
+--     dbms_output.put_line('2');
+--     end if;
+--end;
+--------------------------------------
+--DECLARE
+--    v_i NUMBER:= 0; 
+--    v_rs number;
+--begin
+--    loop            
+--        v_i:=v_i+1;
+--        v_rs:= 19*v_i;
+--        dbms_output.put_line('i: '||v_i||'rs: '|| v_rs);
+--        if v_i>=5 then
+--            exit;
+--        end if;
+--    end loop;
+--end;
+--------------------------------------
+--DECLARE
+--    v_i NUMBER:= 0; 
+--    v_rs number;
+--begin
+--   while v_i>=5 loop            
+--        v_i:=v_i+1;
+--        v_rs:= 19*v_i;
+--        dbms_output.put_line('i: '||v_i||'rs: '|| v_rs);
+--    end loop;
+--end;
+--------------------------------------
+--DECLARE
+--    v_i NUMBER; 
+--    v_rs number;
+--begin
+--  for v_i in 1 .. 10 loop            
+--        v_rs:= 19*v_i;
+--        dbms_output.put_line('i: '||v_i||'rs: '|| v_rs);
+--    end loop;
+--end;
+--------------------------------------
+--create or replace trigger i_employee
+--before insert on employees
+--for each row
+--enable
+--begin 
+--    dbms_output.put_line('insert new item');
+--end;
+--------------------------------------
